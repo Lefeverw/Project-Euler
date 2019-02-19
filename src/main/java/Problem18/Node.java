9293e50c4c9ab12f880b1f8248429548e6ccfeab
lefeverw <wouter.lefever@tomtom.com>
2019-02-19 22:35:41 +0100

    Problem18

---
+++ src/main/java/Problem18/Node.java
@@ -0,0 +1,64 @@
+package Problem18;
+
+import java.util.ArrayList;
+
+/**
+ * @author lefeverw
+ */
+public class Node {
+    int row;
+    int column;
+    int number;
+    int maxDistance;
+    public Node(int row, int column, int number) {
+        this.row = row;
+        this.number=number;
+        this.column = column;
+    }
+
+
+    public void calculateMaxDistance(ArrayList<Node> listNodes) {
+        if(row==0)
+            maxDistance=number;
+        else{
+            maxDistance=listNodes.stream()
+                    .filter(node -> node.getRow()==row-1 && (node.getColumn()==column||node.getColumn()==column-1))
+                    .mapToInt(node->node.getMaxDistance()+number)
+                    .max()
+                    .getAsInt();
+        }
+
+    }
+
+    public int getRow() {
+        return row;
+    }
+
+    public void setRow(int row) {
+        this.row = row;
+    }
+
+    public int getColumn() {
+        return column;
+    }
+
+    public void setColumn(int column) {
+        this.column = column;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
+
+    public int getMaxDistance() {
+        return maxDistance;
+    }
+
+    public void setMaxDistance(int maxDistance) {
+        this.maxDistance = maxDistance;
+    }
+}
---
+++ src/main/java/Problem67/MainProblem67.java
@@ -0,0 +1,32 @@
+package Problem67;
+
+import com.google.common.collect.Lists;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.stream.IntStream;
+
+/**
+ * @author lefeverw
+ */
+public class MainProblem67 {
+    public static void main(String[] args) throws IOException {
+        AtomicInteger row = new AtomicInteger();
+        ArrayList<Node> listNodes = Lists.newArrayList();
+        Files.lines(Paths.get("C:\\Users\\lefeverw\\Documents\\GitHub\\Project-Euler\\src\\main\\java\\Problem67\\tree.txt"))
+                .map(line -> line.split(" "))
+                .forEach(rowElements -> {
+                    IntStream.range(0, rowElements.length)
+                            .forEach(column -> {
+                                Node node =new Node(row.intValue(), column, Integer.parseInt(rowElements[column]));
+                                listNodes.add(node);
+                                node.calculateMaxDistance(listNodes);
+                            });
+                    row.getAndIncrement();
+                });
+        System.out.println(listNodes.stream().mapToInt(Node::getMaxDistance).max().getAsInt());
+    }
+}
